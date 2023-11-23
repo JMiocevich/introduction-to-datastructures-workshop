edb77c5e6d43c66c39ebed7159ddb0be85df964a
Joseph Miocevich <jmiocevich@gmail.com>
2023-11-23 11:11:49 +0800

    add queue

---
+++ src/queue.ts
@@ -0,0 +1,23 @@
+export class Queue<T> {
+    public length: number
+
+    constructor() {
+    }
+
+    enqueue(item: T): void {
+
+    }
+
+    dequeue(): T | undefined {
+
+    }
+
+    peek(): T | undefined {
+
+    }
+
+    isEmpty(): boolean {
+
+    }
+
+}
