e6cbe74f085a1b7111f2f48fc14bfc6f996484c4
Joseph Miocevich <jmiocevich@gmail.com>
2024-02-25 21:30:06 +0800

    queue

--- src/queue.ts
+++ src/queue.ts
@@ -1,23 +1,53 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
 export class Queue<T> {
     public length: number
+    private head?: Node<T>
+    private tail?: Node<T>
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0
     }
 
     enqueue(item: T): void {
+        this.length++
+        const node = { value: item } as Node<T>
+        if (!this.tail) {
+            this.tail = this.head = node
+        }
+
+        this.tail.next = node
+        this.tail = node
 
     }
 
     dequeue(): T | undefined {
 
+        if (!this.head) {
+            return undefined
+        }
+
+        this.length--
+        const head = this.head
+        this.head = this.head.next
+
+        return head.value
+
     }
 
     peek(): T | undefined {
-
+        return this.head?.value
     }
 
     isEmpty(): boolean {
-
+        if (this.length === 0) {
+            return false
+        }
+        return true
     }
 
 }
