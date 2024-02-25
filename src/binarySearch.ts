2fe04fc8236cd36123ea0b9c71b8a82937b1db68
Joseph Miocevich <jmiocevich@gmail.com>
2024-02-25 21:13:19 +0800

    bfs

--- src/binarySearch.ts
+++ src/binarySearch.ts
@@ -1,5 +1,26 @@
+
+
 export function binarySearch(haystack: number[], needle: number): boolean {
 
 
+    let lo = 0
+    let hi = haystack.length
+    while (lo < hi) {
+
+        const m = Math.floor(lo + (hi - lo) / 2)
+        const v = haystack[m]
+
+        if (v === needle) {
+            return true
+        } else if (needle > v) {
+            lo = m + 1
+        } else {
+            hi = m
+        }
+
+
+    }
+
+    return false
 
 }
