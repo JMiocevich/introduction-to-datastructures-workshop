a1d01dd1d4e81e95b40e7381c31c69f33c09b466
Joseph Miocevich <jmiocevich@gmail.com>
2024-03-01 17:31:58 +0800

    update

--- src/mazeSolver.ts
+++ src/mazeSolver.ts
@@ -1,23 +1,19 @@
 
 const dir = [
-    [-1, 0],
     [1, 0],
+    [-1, 0],
     [0, -1],
     [0, 1]
 ]
 
 function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
 
-
-    // base case
-    // off the map
+    // base cases
 
     if (curr.x < 0 || curr.x >= maze[0].length ||
         curr.y < 0 || curr.y >= maze.length) {
         return false
     }
-
-    // on a wall
 
     if (maze[curr.y][curr.x] === wall) {
         return false
@@ -34,46 +30,49 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
     }
 
 
-    // pre
-    seen[curr.y][curr.x] = true
     path.push(curr)
+    seen[curr.y][curr.x] = true
 
-    // recurse
+    // recursive
 
     for (let i = 0; i < dir.length; i++) {
+
         const [x, y] = dir[i]
 
-        if (walk(maze, wall,
-            {
+        if (
+            walk(maze, wall, {
                 x: curr.x + x,
                 y: curr.y + y,
-            }, end, seen, path)) {
-
+            },
+                end, seen, path)) {
             return true
         }
 
 
+
     }
 
+    // post 
 
-    // post
     path.pop()
 
-    return false
+
+
+
+
 }
 
 
 export function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
-    const seen: boolean[][] = []
     const path: Point[] = []
 
-    for (let i = 0; i < maze.length; i++) {
-        seen.push(new Array(maze[0].length).fill(false))
-    }
+    const seen = maze.map(row => new Array(row.length).fill(false));
+
 
     walk(maze, wall, start, end, seen, path)
 
     return path
+
 
 }
