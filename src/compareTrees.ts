e4fe200111d78a63e07f1cf8015300fbfbcc5103
Joseph Miocevich <jmiocevich@gmail.com>
2024-03-01 18:34:26 +0800

    commit

--- src/compareTrees.ts
+++ src/compareTrees.ts
@@ -1,3 +1,18 @@
 export function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+    // compare bnary trees
+    if (!a && !b) {
+        return true
+    }
+
+    if (!a || !b) {
+        return false
+    }
+
+    if (a.value !== b.value) {
+        return false
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right)
+
 }
