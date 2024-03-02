1ef7ceaf2efadd1055ecff646893d0f155b2cdd1
Joseph Miocevich <jmiocevich@gmail.com>
2024-03-02 14:06:50 +0800

    fix

--- src/mazeSolver.ts
+++ src/mazeSolver.ts
@@ -9,7 +9,6 @@ const dir = [
 function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
 
     // base cases
-
     if (curr.x < 0 || curr.x >= maze[0].length ||
         curr.y < 0 || curr.y >= maze.length) {
         return false
@@ -18,7 +17,6 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
     if (maze[curr.y][curr.x] === wall) {
         return false
     }
-
 
     if (curr.y === end.y && curr.x === end.x) {
         path.push(end)
@@ -29,16 +27,13 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
         return false
     }
 
-
     path.push(curr)
     seen[curr.y][curr.x] = true
 
     // recursive
 
     for (let i = 0; i < dir.length; i++) {
-
         const [x, y] = dir[i]
-
         if (
             walk(maze, wall, {
                 x: curr.x + x,
@@ -47,19 +42,13 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
                 end, seen, path)) {
             return true
         }
-
-
-
     }
 
-    // post 
+    // post recursion
 
     path.pop()
 
-
-
-
-
+    return false
 }
 
 
